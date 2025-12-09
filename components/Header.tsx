af83ff4f72aefd075d24dd941918cb2c37c2854d
Do Quoc Tien <hadesdqt@gmail.com>
2025-12-09 14:19:36 +0700

    custom code header part 2

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,19 +2,32 @@ import React from "react";
 import Container from "./Container";
 import Logo from "./Header/Logo";
 import HeaderMenu from "./Header/HeaderMenu";
+import SearchBar from "./Header/SearchBar";
+import CartIcon from "./Header/CartIcon";
+import FavoriteButton from "./Header/FavoriteButton";
+import SignIn from "./Header/SignIn";
+import MobileMenu from "./Header/MobileMenu";
 
 const Header = () => {
   return (
     <header className="bg-white py-5 border-b border-b-black/20">
-      <Container className="flex items-center justify-between">
-        {/* Logo */}
-        <Logo />
+      <Container className="flex items-center justify-between  text-lightColor">
+        <div className="w-auto md:w-1/3 flex items-center gap-2.5 justify-start md:gap-0">
+          {/* Logo */}
+          <MobileMenu />
+          <Logo />
+        </div>
 
         {/* NavButton  */}
         <HeaderMenu />
 
-        <div>Others</div>
-        {/* NavAdmin */}
+        {/*Other */}
+        <div className="w-auto md:w-1/3 flex items-center justify-end gap-5">
+          <SearchBar />
+          <CartIcon />
+          <FavoriteButton />
+          <SignIn />
+        </div>
       </Container>
     </header>
   );
---
+++ components/Header/SignIn.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+  return (
+    <button className="text-sm font-semibold hover:text-darkColor text-lightColor hover:cursor-pointer hoverEffect">
+      Đăng nhập
+    </button>
+  );
+};
+
+export default SignIn;
